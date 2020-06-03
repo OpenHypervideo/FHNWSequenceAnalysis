be22766db2d3ed404f068e7deaaaedded27ac164
alexabruck <5918060+alexabruck@users.noreply.github.com>
2020-06-03 21:02:09 +0200

    Detect two sequences as proof of concept

--- js/methods.detection.js
+++ js/methods.detection.js
@@ -1,46 +1,77 @@
 function analyseSequences() {
-	for (var i = 0; i < sequenceData.length; i++) {
-		var detectedSequences = detectSequences(sequenceData[i].actions);
-		sequenceData[i].sequences = detectedSequences;
-	}
+  console.log("sequenceData BEFORE", sequenceData);
+  for (var i = 0; i < sequenceData.length; i++) {
+    var detectedSequences = detectSequences(sequenceData[i].actions);
+    sequenceData[i].sequences = detectedSequences;
+  }
+  console.log("sequenceData AFTER", sequenceData);
+  updateVisualResult(sequenceData);
+  updateDataTextarea(sequenceData);
+}
 
-	updateVisualResult(sequenceData);
-	updateDataTextarea(sequenceData);
+function encodeActionArray(actionArray) {
+  var ACTIONS = {
+    VideoPlay: "A",
+    VideoPause: "B",
+    VideoJumpBackward: "C",
+    VideoJumpForward: "D",
+    AnnotationAdd: "E",
+    AnnotationChangeText: "F",
+    AnnotationChangeTime: "G",
+  };
+  return actionArray
+    .map(({ Aktion }) => Aktion)
+    .map((actionName) => ACTIONS[actionName] || "X")
+    .join("");
+}
+
+function findMatchesWithStartEnd(string, regex) {
+  var matches = [];
+  while ((match = regex.exec(string)) != null) {
+    matches.push({
+      match: match[0],
+      actionIndexFrom: match.index,
+      actionIndexTo: match.index + match[0].length - 1,
+    });
+  }
+  return matches;
 }
 
 function detectSequences(actionArray) {
+  var sequenceResults = [];
+  var encodedActionArray = encodeActionArray(actionArray);
 
-	console.log(actionArray);
+  var patterns = [
+    {
+      name: "1.1",
+      sequenceLabel: "Search position and add annotation",
+      regex: /([AB](?=.*[CD].*E)[^EFG]{1,3}E)|([CD][^EFG]{1,3}E)/g,
+    },
+    {
+      name: "2.1",
+      sequenceLabel: "Search to adjust annotation time",
+      regex: /([AB](?=.*[CD].*G)[^EF]{3}G)|([CD][^EF]{3}G)/g,
+    },
+  ];
+  for (pattern of patterns) {
+    var matches = findMatchesWithStartEnd(encodedActionArray, pattern.regex);
+    for (match of matches) {
+      sequenceResults.push({
+        label: pattern.sequenceLabel,
+        number: pattern.name,
+        actionIndexFrom: match.actionIndexFrom,
+        actionIndexTo: match.actionIndexTo,
+      });
+    }
+  }
 
-	var sequenceResults = [];
-
-	/**** 
-	* DUMMY SEQUENCE DETECTION 
-	* (add dummy sequence for all action arrays which start with the action "VideoPlay"
-	*  and contain at least 3 actions)
-	****/
-	if (actionArray[0]['Aktion'] == 'VideoPlay' && actionArray.length >= 3) {
-		
-		var randomRule = sequenceRules[getRandomInt(0,19)];
-
-		sequenceResults.push({
-			label: randomRule.sequenceLabel,
-			number: randomRule.sequenceNumber,
-			actionIndexFrom: getRandomInt(0,2),
-			actionIndexTo: getRandomInt(4,6)
-		});
-
-	}
-
-	return sequenceResults;
-
+  return sequenceResults;
 }
 
 function getRules() {
-	getSheetData('1bXfSB9tCW_0-5X_4Swmxl-sZ9Ivn5WgzmpBztZTbx8g', function(ruleSheetData) {
-		
-		console.log(ruleSheetData);
-		sequenceRules = ruleSheetData;
-
-	});
+  getSheetData("1bXfSB9tCW_0-5X_4Swmxl-sZ9Ivn5WgzmpBztZTbx8g", function (
+    ruleSheetData
+  ) {
+    sequenceRules = ruleSheetData;
+  });
 }
